5b44fd41b02502baedebec1a53caf8baff420af7
leicer42 <leicer42@gmail.com>
2022-05-21 22:16:56 -0300

    Create pages and resolving bugs

---
+++ src/pages/Home.tsx
@@ -0,0 +1,63 @@
+import { useNavigate } from 'react-router-dom';
+import { auth, firebase } from '../services/firebase'
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+import '../styles/button.scss';
+
+// import { Button } from '../components/Button';
+
+export function Home() {
+
+  const navigate = useNavigate();
+
+  function handleCreateRoom() {
+    // navigate('/rooms/new');
+
+    const provider = new firebase.auth.GithubAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+    })
+
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração perguntas e respostas" />
+
+        <strong>Crie salas Q&amp;A ao vivo</strong>
+        <p>Tire suas duvidas em tempo real!</p>
+
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Correio deselegante" />
+
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+
+            <button className="button" type="submit">
+              Entrar na sala
+            </button>
+
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import '../styles/auth.scss';
+import '../styles/button.scss';
+
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração perguntas e respostas" />
+
+        <strong>Crie salas Q&amp;A ao vivo</strong>
+        <p>Tire suas duvidas em tempo real!</p>
+
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Correio deselegante" />
+
+          <h2>Criar uma nova sala</h2>
+
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+
+            <button className="button" type="submit">
+              Criar sala
+            </button>
+
+          </form>
+
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+
+        </div>
+      </main>
+    </div>
+  )
+}
